0cfa35b3afa40bd8807557f75987e160837aaf2d
ebeeharry <111122181+ebeeharry@users.noreply.github.com>
2023-03-18 20:56:45 +0400

    Add files via upload

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     return (
         <nav className='nav'>
             <ul className='nav_list'>
-                <li className={currentRoute === '/Kasa/' ? 'nav_list_item_active' : 'nav_list_item'}>
+                <li className={currentRoute === '/' ? 'nav_list_item_active' : 'nav_list_item'}>
                     <Link  to='/'>
                         Accueil
                     </Link>
